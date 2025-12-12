cdd94c32e7c7b2f76b85818b777b0c79e113c69c
Nizar Beriane <nizarberiane@proton.me>
2025-12-12 13:40:54 +0100

    feat: configure dev environment and enhance auth integration [CF-2, CF-3]
    - Update dev server port to 5173
    - Refine API interceptors and Auth Service
    - Update Auth Types to match backend response
    - Configure Prettier and ESLint
    - Apply formatting fixes

--- src/services/auth/auth.service.ts
+++ src/services/auth/auth.service.ts
@@ -1,20 +1,28 @@
 import api from "../api";
-import type { LoginRequest, LoginResponse, RegisterRequest, RegisterResponse, LogoutResponse } from "../../types/auth.types";
+import type {
+  LoginRequest,
+  LoginResponse,
+  RegisterRequest,
+  RegisterResponse,
+  LogoutResponse,
+} from "../../types/auth.types";
 
 const authService = {
-    login: async (loginRequest: LoginRequest) => {
-        const response = await api.post<LoginResponse>("/auth/login", loginRequest);
-        return response.data;
-    },
-    register: async (registerRequest: RegisterRequest) => {
-        const response = await api.post<RegisterResponse>("/auth/register", registerRequest);
-        return response.data;
-    },
-    logout: async () => {
-        const response = await api.post<LogoutResponse>("/auth/logout");
-        return response.data;
-    },
-
+  login: async (loginRequest: LoginRequest) => {
+    const response = await api.post<LoginResponse>("/auth/login", loginRequest);
+    return response.data;
+  },
+  register: async (registerRequest: RegisterRequest) => {
+    const response = await api.post<RegisterResponse>(
+      "/auth/register",
+      registerRequest,
+    );
+    return response.data;
+  },
+  logout: async () => {
+    const response = await api.post<LogoutResponse>("/auth/logout");
+    return response.data;
+  },
 };
 
 export default authService;
--- src/types/auth.types.ts
+++ src/types/auth.types.ts
@@ -1,45 +1,51 @@
 interface LoginResponse {
-    message: string;
-    token: string;
-    data: {
-        name: string;
-        email: string;
-        role: 'user' | 'admin' | 'driver';
-        status: 'active' | 'inactive';
-        profileImage?: string;
-        _id: string;
-        createdAt: string;
-        updatedAt: string;
-        __v: number;
-    }
-}
-interface LoginRequest {
-    email: string;
-    password: string;
-}
-interface RegisterRequest {
+  message: string;
+  token: string;
+  data: {
     name: string;
     email: string;
-    password: string;
-    role: 'user' | 'admin' | 'driver';
+    role: "user" | "admin" | "driver";
+    status: "active" | "inactive";
     profileImage?: string;
+    _id: string;
+    createdAt: string;
+    updatedAt: string;
+    __v: number;
+  };
+}
+interface LoginRequest {
+  email: string;
+  password: string;
+}
+interface RegisterRequest {
+  name: string;
+  email: string;
+  password: string;
+  role: "user" | "admin" | "driver";
+  profileImage?: string;
 }
 interface RegisterResponse {
-    message: string;
-    token: string;
-    data: {
-        name: string;
-        email: string;
-        role: 'user' | 'admin' | 'driver';
-        status: 'active' | 'inactive';
-        profileImage?: string;
-        _id: string;
-        createdAt: string;
-        updatedAt: string;
-        __v: number;
-    }
+  message: string;
+  token: string;
+  data: {
+    name: string;
+    email: string;
+    role: "user" | "admin" | "driver";
+    status: "active" | "inactive";
+    profileImage?: string;
+    _id: string;
+    createdAt: string;
+    updatedAt: string;
+    __v: number;
+  };
 }
 interface LogoutResponse {
-    message: string;
+  message: string;
 }
-export type { LoginResponse, LoginRequest, RegisterRequest, RegisterResponse, LogoutResponse };
+export type {
+  LoginResponse,
+  LoginRequest,
+  RegisterRequest,
+  RegisterResponse,
+  LogoutResponse,
+};
